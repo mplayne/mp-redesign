3d27c73253a247a9d4aada529b6880c256daa1a9
Matt Playne <mattplayne@gmail.com>
2022-09-20 20:39:45 -0400

    upcoming shows added

--- src/components/UpcomingShows.js
+++ src/components/UpcomingShows.js
@@ -1,1 +1,32 @@
-export default function UpcomingShows() {}
+
+
+export default function UpcomingShows() {
+    return(
+        <section>
+            <hr />
+            <h2 className="text-center mb-4">Upcoming Shows</h2>
+            <a className="text-decoration-none show-text" href="/">
+                <ul className="center-list d-flex justify-content-between">
+                    <li>01/21/2023</li>
+                    <li>Midland Public Library</li>
+                    <li>Midland,ON</li> 
+                </ul>
+                </a>
+                <a className="text-decoration-none show-text" href="/">
+                <ul className="center-list d-flex justify-content-between">
+                    <li>01/21/2023</li>
+                    <li>Midland Public Library</li>
+                    <li>Midland,ON</li> 
+                </ul>
+                </a>
+                 <a className="text-decoration-none show-text" href="/">
+                <ul className="center-list d-flex justify-content-between">
+                    <li>01/21/2023</li>
+                    <li>Midland Public Library</li>
+                    <li>Midland,ON</li> 
+                </ul>
+                </a>
+            <hr />
+         </section>
+    )
+}
