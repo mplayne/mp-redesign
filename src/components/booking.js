667e09b7ac9350382fe87746627545ba3f2fe4fe
Matt Playne <mattplayne@gmail.com>
2022-09-23 13:48:36 -0400

    booking form with spinner added

--- src/components/booking.js
+++ src/components/booking.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Spinner 
+from 'react-bootstrap/Spinner'
+
 
 export default function Booking() {
     return (
-        <h1>This is the booking page</h1>
+        <Container>
+        
+            <div className="text-center mb-5 if-spinner">
+            <iframe data-tally-src="https://tally.so/embed/3jyW4m?hideTitle=1&dynamicHeight=1" width="100%" height="1321" frameborder="0" marginheight="0" marginwidth="0" title="Booking Form"></iframe>
+            </div>
+            
+        </Container>
+    
+        
     )
 }
+
 
