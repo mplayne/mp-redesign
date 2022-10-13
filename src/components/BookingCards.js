321575f8fb5b21d871b5b86d0fdeb916cf9b3aa9
home <200430607@student.georgianc.on.ca>
2022-10-13 15:46:32 -0400

    upcoming shows placeholder links commented out, replaced with button linking to booking page

--- src/components/BookingCards.js
+++ src/components/BookingCards.js
@@ -1,36 +1,51 @@
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Container from 'react-bootstrap/Container';
-
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import Container from "react-bootstrap/Container";
 
 export default function BookingCards() {
-    return (
-        <Container className="booking-cards">
-            <h1 className="d-flex justify-content-center ">Book The Best In Town</h1>
-        <CardGroup className="gap-4 mt-4" >
-      
-            <Card className="text-center card-bg" text="light">
-            <Card.Img  className="card-img" variant="top" src="/images/live.jpg" />
-            <Card.Body className="d-flex align-items-center justify-content-center">
-              <a href="/" className="stretched-link text-decoration-none card-text"><Card.Title>Private Events</Card.Title></a>
-            </Card.Body>
-          </Card>
+  return (
+    <Container className="booking-cards">
+      <h1 className="d-flex justify-content-center ">Book The Best In Town</h1>
+      <CardGroup className="gap-4 mt-4">
+        <Card className="text-center card-bg" text="light">
+          <Card.Img className="card-img" variant="top" src="/images/live.jpg" />
+          <Card.Body className="d-flex align-items-center justify-content-center">
+            <a
+              href="/booking"
+              className="stretched-link text-decoration-none card-text"
+            >
+              <Card.Title>Private Events</Card.Title>
+            </a>
+          </Card.Body>
+        </Card>
 
-          <Card className="text-center card-bg" text="light" >
-            <Card.Img className="card-img" variant="top" src="/images/cafe.jpg" />
-            <Card.Body className="d-flex align-items-center justify-content-center">
-            <a href="/" className="stretched-link text-decoration-none card-text">
-              <Card.Title>Bars &middot; Restaurants &middot; Cafes</Card.Title></a>
-            </Card.Body>
-          </Card>
-          <Card className="text-center rounded card-bg" text="light">
-            <Card.Img className="card-img" variant="top" src="/images/wedding.jpg" />
-            <Card.Body className="d-flex align-items-center justify-content-center">
-            <a href="/" className="stretched-link text-decoration-none card-text">
-              <Card.Title>Weddings</Card.Title></a>
-            </Card.Body>
-          </Card>
-        </CardGroup>
-        </Container>
-      );
+        <Card className="text-center card-bg" text="light">
+          <Card.Img className="card-img" variant="top" src="/images/cafe.jpg" />
+          <Card.Body className="d-flex align-items-center justify-content-center">
+            <a
+              href="/booking"
+              className="stretched-link text-decoration-none card-text"
+            >
+              <Card.Title>Bars &middot; Restaurants &middot; Cafes</Card.Title>
+            </a>
+          </Card.Body>
+        </Card>
+        <Card className="text-center rounded card-bg" text="light">
+          <Card.Img
+            className="card-img"
+            variant="top"
+            src="/images/wedding.jpg"
+          />
+          <Card.Body className="d-flex align-items-center justify-content-center">
+            <a
+              href="/booking"
+              className="stretched-link text-decoration-none card-text"
+            >
+              <Card.Title>Weddings</Card.Title>
+            </a>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+    </Container>
+  );
 }
--- src/components/UpcomingShows.js
+++ src/components/UpcomingShows.js
@@ -1,24 +1,30 @@
 import { Container } from "react-bootstrap";
-
+import Button from "react-bootstrap/Button";
 
 export default function UpcomingShows() {
-    return(
-        <Container className="upcoming-shows">
-            <h3 className="text-center mb-4">Upcoming Shows</h3>
-            <a className="text-decoration-none show-text" href="/">
-                <ul className="center-list d-flex justify-content-between">
-                    <li>01/21/2023</li>
-                    <li>Midland Public Library</li>
-                    <li>Midland,ON</li> 
-                </ul>
-                </a>
-                <a className="text-decoration-none show-text" href="/">
-                <ul className="center-list d-flex justify-content-between">
-                    <li>01/21/2023</li>
-                    <li>Midland Public Library</li>
-                    <li>Midland,ON</li> 
-                </ul>
-                </a>
-         </Container>
-    )
+  return (
+    <Container className="upcoming-shows">
+      <h3 className="text-center mb-4">Upcoming Shows</h3>
+      <div className="d-flex justify-content-center">
+        <Button
+          className="text-center d-flex align-item-center"
+          variant="warning"
+        >
+          <a className="book-btn" href="/booking">
+            Book Live Music
+          </a>
+        </Button>
+      </div>
+
+      {/* Individual Show List Item */}
+
+      {/* <a className="text-decoration-none show-text" href="/">
+        <ul className="center-list d-flex justify-content-between">
+          <li>01/21/2023</li>
+          <li>Midland Public Library</li>
+          <li>Midland,ON</li>
+        </ul>
+      </a> */}
+    </Container>
+  );
 }
