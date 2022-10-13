25ca92f4d7084e3bff9866bc1d85d8fc5ac5f414
Matt Playne <mattplayne@gmail.com>
2022-10-13 14:48:10 -0400

    shows hover yellow

--- src/components/BookingCards.js
+++ src/components/BookingCards.js
@@ -7,12 +7,12 @@ export default function BookingCards() {
     return (
         <Container className="booking-cards">
             <h1 className="d-flex justify-content-center ">Book The Best In Town</h1>
-        <CardGroup className="gap-3 mt-4" >
+        <CardGroup className="gap-4 mt-4" >
       
             <Card className="text-center card-bg" text="light">
             <Card.Img  className="card-img" variant="top" src="/images/live.jpg" />
             <Card.Body className="d-flex align-items-center justify-content-center">
-              <a href="/" className="stretched-link text-decoration-none card-text"><Card.Title >Private Events</Card.Title></a>
+              <a href="/" className="stretched-link text-decoration-none card-text"><Card.Title>Private Events</Card.Title></a>
             </Card.Body>
           </Card>
 
@@ -25,9 +25,9 @@ export default function BookingCards() {
           </Card>
           <Card className="text-center rounded card-bg" text="light">
             <Card.Img className="card-img" variant="top" src="/images/wedding.jpg" />
-            <Card.Body className="card-body">
+            <Card.Body className="d-flex align-items-center justify-content-center">
             <a href="/" className="stretched-link text-decoration-none card-text">
-              <Card.Title className="big">Weddings</Card.Title></a>
+              <Card.Title>Weddings</Card.Title></a>
             </Card.Body>
           </Card>
         </CardGroup>
--- src/components/UpcomingShows.js
+++ src/components/UpcomingShows.js
@@ -19,14 +19,6 @@ export default function UpcomingShows() {
                     <li>Midland,ON</li> 
                 </ul>
                 </a>
-                 <a className="text-decoration-none show-text" href="/">
-                <ul className="center-list d-flex justify-content-between">
-                    <li>01/21/2023</li>
-                    <li>Midland Public Library</li>
-                    <li>Midland,ON</li> 
-                </ul>
-                </a>
-            <hr />
          </Container>
     )
 }
